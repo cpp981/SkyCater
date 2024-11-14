01faf91859e049c6d7a3f25d01582ee2b915fc69
cpp <cpp.daw@gmail.com>
2024-11-14 20:47:57 +0100

    add: logo y logo-menu. Botón exportar a PDF tabla

--- public/inventario.php
+++ public/inventario.php
@@ -15,8 +15,16 @@ if (!isset($_SESSION['nombre'])) {
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"
         integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <!-- css Bootstrap CDN -->
+
+    <!-- CSS de DataTables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
+
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS de Buttons para DataTables -->
+    <link rel="stylesheet" type="text/css"
+        href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
     <!-- css Fontawesome CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!--JSDelivr SweetAlert2 CDN -->
@@ -25,8 +33,6 @@ if (!isset($_SESSION['nombre'])) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf/notyf.min.css">
     <!-- Script de Notyf -->
     <script src="https://cdn.jsdelivr.net/npm/notyf/notyf.min.js"></script>
-    <!-- css DataTables CDN -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="../css/styles.css">
     <script type="text/javascript" src="../js/inventario.js"></script>
 </head>
@@ -34,8 +40,13 @@ if (!isset($_SESSION['nombre'])) {
 <body class="d-flex flex-column"> <!--style="height: 100vh;" -->
     <div class="super d-flex flex-grow-1">
         <div class="sidebar rounded-bottom shadow" id="sidebar">
-            <i id="cloud-logo" class="fas fa-2x fa-cloud mt-3"></i>
-            <h4 class="text-white text-center mt-2">SkyCater</h4>
+        <div class="d-flex justify-content-center mt-3">
+                <!-- Logo de la aplicación -->
+                <a  class="navbar-brand">
+                    <img src="../img/LogoMenu.png" alt="Logo" class="rounded img-fluid logo-menu">
+                    <p class="text-white text-center mt-2"><i class="fas fa-user text-white me-1"></i><?php echo $_SESSION['nombre']?></p>
+                </a>
+            </div>
             <ul class="nav flex-column mt-5">
                 <li class="nav-item">
                     <a class="nav-link text-white" href="index.php"><i class="fas fa-dashboard"></i> Dashboard</a>
@@ -53,7 +64,7 @@ if (!isset($_SESSION['nombre'])) {
                 </li>
             </ul>
             <div class="d-flex justify-content-center align-items-end mt-5">
-                <a class="nav-link text-white"><i class="fas fa-lg fa-moon"></i> Modo Oscuro</a>
+               <!-- <a class="nav-link text-white"><i class="fas fa-lg fa-moon"></i> Modo Oscuro</a>-->
             </div>
         </div>
         <div class="flex-grow-1 d-flex flex-column">
@@ -66,14 +77,14 @@ if (!isset($_SESSION['nombre'])) {
                     <p class="mx-2" style="color:#003262; margin-bottom: 0;">Bienvenido,</p>
                     <input type="text" class="form-control d-inline-block w-25" id="inputId"
                         value="<?php echo $_SESSION['nombre']; ?>" disabled>
-                    <button id="close" class="btn text-white shadow" style="background-color: #003262;">
+                    <button id="close" class="btn text-white shadow btn-danger">
                         <i class="fas fa-sign-out-alt"></i> Cerrar Sesión
                     </button>
                 </div>
             </nav>
             <div id="containerTablaProducto"
                 class="container-fluid flex-grow-1 d-flex flex-column justify-content-center mt-4">
-                <div class="text-center mt-5 mb-5" style="color: #003262;">
+                <div class="text-center  mb-5" style="color: #003262;">
                     <h2>Gestión de Productos</h2>
                 </div>
                 <div id="contenedorDataTable" class="flex-grow-1">
@@ -81,6 +92,8 @@ if (!isset($_SESSION['nombre'])) {
                         data-bs-target="#exampleModal">
                         <i class="fas fa-plus"></i> Añadir Producto
                     </a>
+                    <a id="exportPdf" class="btn btn-refresh btn-danger shadow me-4 rounded">
+                        <i class="fas fa-file-pdf"></i> Exportar a PDF</a>
                     <a id="refrescarTabla" class="btn btn-primary shadow rounded">
                         <i class="fas fa-redo"></i></a>
                     <!-- Modal -->
@@ -185,6 +198,17 @@ if (!isset($_SESSION['nombre'])) {
     <!-- JS DataTables CDN -->
     <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
+    <!-- Buttons JS -->
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <!-- PDFMake (necesario para la exportación a PDF en DataTables) -->
+    <script type="text/javascript" charset="utf8"
+        src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" charset="utf8"
+        src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+
+    <!-- Exportación a HTML5 y Print (para PDF) -->
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
 </body>
 <div class="foot d-flex justify-content-center align-items-end mt-5">
     <footer class="fixed-bottom text-center">
